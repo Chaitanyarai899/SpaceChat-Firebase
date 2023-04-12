b3059be2575d8038b4e752bd13f378e6bdd66379
Chaitanya Rai <95732261+Chaitanyarai899@users.noreply.github.com>
2023-04-12 23:54:55 +0530

    email password authentication added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,36 +1,10 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { UserAuth } from "../context/AuthContext";
-import "./component.css";
-
+import React from 'react'
+import './components.css'
 
 const Login = () => {
-  const navigate = useNavigate();
-  const { currentUser, signinWithGoogle } = UserAuth();
-
-  const handleLogin = async () => {
-    try {
-      await signinWithGoogle();
-    } catch(error) {
-      console.log(error)
-    }
-  }
-
-  useEffect(() => {
-    if(currentUser) {
-      navigate("/chat")
-    }
-  }, [currentUser]);
-
   return (
-    <div className="hero min-h-screen bg-base-200" style={{ 
-      backgroundImage: `url(https://images3.alphacoders.com/899/899272.jpg)`, 
-      backgroundSize: 'cover', 
-      backgroundPosition: 'center',
-      backgroundAttachment: 'fixed'
-    }}>
     
-    <div className="hero min-h-screen bg-base-200" style={{ backgroundImage: `url(https://images2.alphacoders.com/912/912958.jpg)`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+           <div className="hero min-h-screen bg-base-200" style={{ backgroundImage: `url(https://images2.alphacoders.com/912/912958.jpg)`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
   <div className="container bg-opacity-50 bg-gray-800">
     <div className="hero-content text-center">
       <div className="max-w-md">
@@ -38,7 +12,27 @@ const Login = () => {
         <p className="py-6 font-bold text-white">
           Multiple chatting room web App where you can meet new space and Sci-fi enthusiasts, and make connections.
         </p>
-        <button type="button" class="btnnnn mx-auto" onClick={handleLogin} style={{ fontSize: '2rem' }}>
+        <div class="card">
+  <div class="card2">
+    <form class="form">
+    <p id="heading">Login</p>
+    <div class="field">
+    <svg viewBox="0 0 16 16" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg" class="input-icon">
+    <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"></path>
+    </svg>
+      <input type="text" class="input-field" placeholder="Username" autocomplete="off"></input>
+    </div>
+    <div class="field">
+    <svg viewBox="0 0 16 16" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg" class="input-icon">
+    <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"></path>
+    </svg>
+      <input type="password" class="input-field" placeholder="Password"></input>
+    </div>
+    <div class="btn">
+    <button class="button1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Login&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
+    <button class="button2">Sign Up</button>
+    </div>
+    <button type="button" class="btnnnn mx-auto"  style={{ fontSize: '2rem' }}>
           <strong>Login With Google</strong>
           <div id="container-stars">
             <div id="stars"></div>
@@ -48,12 +42,13 @@ const Login = () => {
             <div class="circle"></div>
           </div>
         </button>
-      </div>
-    </div>
+</form>
   </div>
-</div>
-</div>
-    );
+    </div>
+      </div>
+       </div>
+     </div>
+    </div>
+  )
 }
-
-export default Login;
+export default Login
