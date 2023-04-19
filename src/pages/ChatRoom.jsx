7a8dd1ea31979fa184172b4bff907bb5a39e25ee
Chaitanya Rai <Chaitanyarai899@gmail.com>
2023-04-20 00:54:18 +0530

    sidenav removed for mobile devices

--- src/pages/ChatRoom.jsx
+++ src/pages/ChatRoom.jsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ChatBox from "../components/ChatBox";
 import SendMessage from "../components/SendMessage";
 import SideNav from "../components/SideNav";
 
 const ChatRoom = () => {
   const [collectionName, setCollectionName] = useState("messages");
+  const [isMobile, setIsMobile] = useState(false);
 
-  const handleChatRoomClick = (name) => {
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768); // adjust the width value to your needs
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const onChatRoomClick = (name) => {
     setCollectionName(name);
   };
 
   return (
     <div className="flex">
-      {/*<SideNav onChatRoomClick={handleChatRoomClick} />*/}
+      {!isMobile && <SideNav onChatRoomClick={onChatRoomClick} />}
       <div className="flex-1">
         <ChatBox collectionName={collectionName} />
         <SendMessage collectionName={collectionName} />
